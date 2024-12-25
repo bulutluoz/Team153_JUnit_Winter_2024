e3b21fed8c9bdaedb0f04f623a59235e2be37211
bulutluoz <abbulutluoz@gmail.com>
2024-12-25 20:57:13 +0100

    day05 sonu

--- src/test/java/tests/day05_jUnit_annotations/C02_OrtakIslemleriMethodlastirma.java
+++ src/test/java/tests/day05_jUnit_annotations/C02_OrtakIslemleriMethodlastirma.java
@@ -1,50 +1,37 @@
 package tests.day05_jUnit_annotations;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
-public class C01_CokluTestOlusturma {
-
+public class C02_OrtakIslemleriMethodlastirma {
     /*
-        1- @Test notasyonu siradan method'lari
-           bagimsiz olarak calisabilir Test Method'lari haline getirir
-
-        2- Bir method'un gecici bir sure icin kullanilmamasini istersek
-           method'u @Disable olarak isaretleyebiliriz
-
-        3- JUnit test method'larini belirli bir kriter olmadan
-           rastgele bir siralama ile calistirir
-           bu siralamayi ONGORMEMIZ veya KONTROL ETMEMIZ mumkun degildir
-           (ekstra bazi notasyonlarla bu saglanabilir)
-
-        4- Eger sirali calismasini istedigimiz test method'lari olursa
-           isimlerini test01, test02, test03 ... yaparsak
-           bu siralamaya uygun olarak test method'larini calistirir
-
+        5- Java OOP consept ayni kodlari tekrar tekrar yazmak yerine
+           method'lastirmamizi tavsiye eder
+           Biz de baslangictaki driver edinme adimlarini
+           ve sondaki kapatmayi ayri bir method olarak kullanabiliriz
      */
+    WebDriver driver;
 
-    // 3 farkli method olusturup asagidaki gorevleri yapin
-    // 1- testotomasyonu sayfasina gidin ve url'in testotomasyonu icerdigini test edin
-    // 2- youtube ana sayfaya gidin ve Title'in YouTube oldugunu test edin
-    // 3- wisequarter anasayfaya gidin ve Title'in case sensitive olmadan wisequarter icerdigini test edin
+    public void setup(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    public void teardown(){
+        driver.quit();
+    }
 
     @Test
     public void testotomasyonuTesti() throws InterruptedException {
-
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
+        setup();
         // 1- testotomasyonu sayfasina gidin
-
         driver.get("https://www.testotomasyonu.com");
 
         // ve url'in testotomasyonu icerdigini test edin
-
         String expectedUrlIcerik = "testotomasyonu";
         String actualUrl = driver.getCurrentUrl();
 
@@ -53,18 +40,13 @@ public class C01_CokluTestOlusturma {
         } else System.out.println("Testotomasyonu Testi FAILED");
 
         Thread.sleep(1000);
-        driver.quit();
-
+        teardown();
     }
 
     // @Disabled  Gecici olarak bu method'un kullanilmamasini istedigimizde
     @Test
     public void youtubeTesti() throws InterruptedException {
-
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
+        setup();
         // 2- youtube ana sayfaya gidin ve
         driver.get("https://www.youtube.com");
 
@@ -78,15 +60,12 @@ public class C01_CokluTestOlusturma {
         } else System.out.println("Youtube testi FAILED");
 
         Thread.sleep(1000);
-        driver.quit();
+        teardown();
     }
 
     @Test
     public void wisequarterTesti() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
+        setup();
         // 3- wisequarter anasayfaya gidin ve
         driver.get("https://www.wisequarter.com");
 
@@ -100,9 +79,6 @@ public class C01_CokluTestOlusturma {
         } else System.out.println("Wisequarter testi FAILED");
 
         Thread.sleep(1000);
-        driver.quit();
-
+        teardown();
     }
-
-
 }
