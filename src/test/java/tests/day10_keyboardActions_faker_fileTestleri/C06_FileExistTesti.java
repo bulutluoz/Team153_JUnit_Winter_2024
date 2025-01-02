3e05655c0165b94392214bea2eb6aedd199355a8
bulutluoz <abbulutluoz@gmail.com>
2025-01-02 20:19:31 +0100

    Ders10 sonu

---
+++ src/test/java/tests/day10_keyboardActions_faker_fileTestleri/C06_FileExistTesti.java
@@ -0,0 +1,44 @@
+package tests.day10_keyboardActions_faker_fileTestleri;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class C06_FileExistTesti {
+
+    @Test
+    public void test01(){
+
+        // day10 package'i altinda deneme.txt diye bir dosya bulundugunu test edin
+
+        // Selenium WebDriver objesi ile kullanilir
+        // yani web sayfalari ile calisir
+        // ancak fiziki olarak bilgisayarimizda bulunan bir dosyaya erismek
+        // ve varligini test etmek icin Java'dan yardim alacagiz
+
+        // Java'nin bir dosyaya ulasabilmesi icin
+        // dosya yoluna ihtiyacimiz var
+
+        String dosyaYolu = "src/test/java/tests/day10_keyboardActions_faker_fileTestleri/deneme.txt";
+
+        System.out.println(  Files.exists(Paths.get(dosyaYolu))  ); // true
+
+
+        String yanlisDosyaYolu = "src/test/java/tests/day10_keyboardActions_faker_fileTestleri/deneme1.txt";
+
+        System.out.println(Files.exists(Paths.get(yanlisDosyaYolu))); // false
+
+
+        // day10 package'i altinda deneme.txt diye bir dosya bulundugunu test edin
+        Assertions.assertTrue(Files.exists(Paths.get(dosyaYolu)));
+
+
+
+
+        // deneme.txt dosyasini downloads klasorune kopyalayin
+        // ve downloads klasorunde deneme.txt dosyasinin varoldugunu test edin
+
+    }
+}
